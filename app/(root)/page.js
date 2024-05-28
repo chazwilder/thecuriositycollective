61fa00b4d8907301438b6e5e1823000518460450
chazwilder <cwilder@chazwilder.onmicrosoft.com>
2024-05-28 15:08:01 -0400

    Post Listing/Archive Item Completed V2!

--- app/(root)/page.js
+++ app/(root)/page.js
@@ -3,11 +3,7 @@ import ArchiveItem from "@/components/blog/ArchiveItem";
 
 export default function Home() {
   return (
-<<<<<<< Updated upstream
-    <main className="flex flex-col mb-auto max-w-6xl w-full mx-auto">
-=======
     <main className="flex flex-col mb-auto max-w-6xl w-full items-center justify-center mx-auto">
->>>>>>> Stashed changes
        <ArchiveItem />
     </main>
   );
